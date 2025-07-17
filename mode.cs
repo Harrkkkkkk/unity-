2a815c66aaabd38323c024579d14079826f31e77
harrytheconqueror <2664281615@qq.com>
2025-07-17 16:08:27 +0800

    Update mode.cs

--- mode.cs
+++ mode.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class mode : MonoBehaviour
 {
+
+    public int health = 5;
+    public float speed =5.5f;
+    public bool IsJump;
+    public GameObject Player;
+    public Transform playermovement;
     // Start is called before the first frame update
     void Start()
     {
